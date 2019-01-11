9703704f1858bcb00ad75f693c74b8fd57050703
Aleksandar Budić <aleksandar.budic@nultien.rs>
2019-01-11 10:05:14 +0100

    ctor guards in BusDriver

--- BusDrivers/BusDriver.cs
+++ BusDrivers/BusDriver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace BusDrivers
@@ -19,7 +20,9 @@ namespace BusDrivers
 
         public BusDriver(Route route, Gossip gossip)
         {
-            _route = route;
+            if (gossip == null)
+                throw new ArgumentNullException(nameof(gossip));
+            _route = route ?? throw new ArgumentNullException(nameof(route));
             _currentStopIndex = 0;
             _gossips = new HashSet<Gossip> { gossip };
         }
